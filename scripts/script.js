e718c4cfbb8bfbca2dcba8b3a6317846646d7a3e
Sonia Chavepeyre <ciarasonia013@gmail.com>
2024-11-06 18:15:26 +0100

    js addEventListener

--- scripts/script.js
+++ scripts/script.js
@@ -6,11 +6,39 @@ function afficherResultat(score, nbMots){
       console.log("Votre score est de : " + score + " sur " + nbMots);
 }
 
-
+function afficherPropositions(proposition){
+  let zoneProposition = document.querySelector(".zoneProposition");
+  zoneProposition.innerText = proposition;
+}
 
 function lancerJeu(){
   let score = 0;
   let nbMots = 0;
+  let i = 0;
+  let boutonValidation = document.getElementById("btnValiderMot");
+  let inputEcriture = document.getElementById("inputEcriture");
 
-  afficherResultat(score, nbMots);
+
+  afficherPropositions(listMots[i]);
+
+  boutonValidation.addEventListener("click", () =>{
+  console.log(inputEcriture.value);
+  if(listMots[i] === inputEcriture.value){
+    score++;
+  }
+  i++;
+  afficherResultat(score, i);
+  inputEcriture.value = "";
+
+  if(listMots[i] === undefined){
+    afficherPropositions("Le jeu est fini");
+    boutonValidation.disabled = true;
+  }else{
+    afficherPropositions(listMots[i]);
+  }
+
+  });
+
+  afficherResultat(score, i);
+
 }
