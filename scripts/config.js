c5d5c4c4a422dacafcbf622c290e02b3fe0605b5
Sonia Chavepeyre <ciarasonia013@gmail.com>
2024-11-21 18:49:12 +0100

    listMot ou listPhrases

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
 // Listes des mots et des phrases
 const listMots = ["Cachalot", "Pétunia", "Serviette"];
-const listPhrases = ["Le cachalot est un mammifère marin.", "Le pétunia est une fleur.", "La serviette est un objet."];
+const listPhrases = ["Le cachalot est un mammifère marin", "Le pétunia est une fleur", "La serviette est un objet"];
--- scripts/script.js
+++ scripts/script.js
@@ -15,29 +15,43 @@ function lancerJeu(){
   let score = 0;
   let nbMots = 0;
   let i = 0;
+  let listeProposition = listMots
   let boutonValidation = document.getElementById("btnValiderMot");
   let inputEcriture = document.getElementById("inputEcriture");
 
 
-  afficherPropositions(listMots[i]);
+  afficherPropositions(listeProposition[i]);
 
   boutonValidation.addEventListener("click", () =>{
   console.log(inputEcriture.value);
-  if(listMots[i] === inputEcriture.value){
+  if(listeProposition[i] === inputEcriture.value){
     score++;
   }
   i++;
   afficherResultat(score, i);
   inputEcriture.value = "";
 
-  if(listMots[i] === undefined){
+  if(listeProposition[i] === undefined){
     afficherPropositions("Le jeu est fini");
     boutonValidation.disabled = true;
   }else{
-    afficherPropositions(listMots[i]);
+    afficherPropositions(listeProposition[i]);
   }
 
   });
+
+  let listBtnRadio = document.querySelectorAll(".optionSource input")
+  for (let index = 0; index < listBtnRadio.length; index++) {
+    listBtnRadio[index].addEventListener("change", (event) => {
+      console.log(event.target.value)
+      if (event.target.value === "1"){
+        listeProposition = listMots
+      } else {
+        listeProposition = listPhrases
+      }
+      afficherPropositions(listeProposition[i])
+    })
+  }
 
   afficherResultat(score, i);
 
