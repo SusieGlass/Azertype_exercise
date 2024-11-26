2c89d46f0542eb17ff9705e66c867bfd0b4548d6
Sonia Chavepeyre <ciarasonia013@gmail.com>
2024-11-26 18:25:44 +0100

    validation email et nom

--- scripts/script.js
+++ scripts/script.js
@@ -18,15 +18,26 @@ function afficherPropositions(proposition){
   zoneProposition.innerText = proposition;
 }
 
-/**
- * Cette fonction construit et affiche l'email.
- * @param {string} nom : le nom du joueur
- * @param {string} email : l'email de la personne avec qui il veut partager son score
- * @param {string} score : le score.
- */
 function afficherEmail(nom, email, score) {
   let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
   location.href = mailto
+}
+
+function validerNom(nom){
+  if (nom.length >= 2) {
+    return true
+  } else {
+    return false
+  }
+}
+
+function validerEmail(email){
+  let emailRegexp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
+  if (emailRegexp.test(email)){
+    return true
+  }else{
+    return false
+  }
 }
 
 function lancerJeu(){
@@ -78,11 +89,14 @@ function lancerJeu(){
     let nom = document.getElementById("nom").value;
     let email = document.getElementById("email").value;
 
-    let scoreEmail = `${score} / ${i}`
+    if (validerEmail(email) && validerNom(nom)){
+      let scoreEmail = `${score} / ${i}`
     afficherEmail(nom, email, scoreEmail)
-    console.log(nom, email);
-
+    }else{
+  console.log("Erreur")
+  }
   })
+
 
   afficherResultat(score, i);
 
